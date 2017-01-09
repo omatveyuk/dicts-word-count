0364617ee090e9a8990ef89f254c117352cb8ceb
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-09 13:02:49 -0800

    Count words exclude punctuation

--- wordcount.py
+++ wordcount.py
@@ -1,20 +1,28 @@
 # put your code here.
+import string 
 
 def count_words(file_name):
     """Count words inside file and prints words
 
-        Return dictionary of the words and counts"""
+        Return dictionary of the words without punctuation and counts"""
 
     input_file = open(file_name)
+
+    #create a list of punctuation
+    exclude = string.punctuation 
 
     #Create dict
     words = {}
     #iterate + + rstrip () + split by spaces
     for line in input_file:
         line = line.rstrip()
+        line = line.lower()
         words_line = line.split()
+        words_line = words_line
     #count each word
         for word in words_line:
+            word = word.rstrip(exclude)
+            word = word.lstrip(exclude)
     # if in dic, increase by 1
             if word in words:
                 words[word] += 1
@@ -29,5 +37,5 @@ def count_words(file_name):
     return words
 
 
-count_words("test.txt")
-#count_words("twain.txt")
+# count_words("test.txt")
+count_words("twain.txt")
